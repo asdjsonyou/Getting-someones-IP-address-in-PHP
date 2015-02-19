776dc49017845c49dbfd86f6015a910f4e7ae8df
asdjsonyou <jhayjhay_02@yahoo.com>
2015-02-19 00:24:00 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+     <title>Getting someones IP address in PHP</title>
+
+</head>
+<body>
+
+<?php
+
+  $ip = $_SERVER['REMOTE_ADDR']; // Grabbing the IP
+  $time = date("l F dS h:i A Y"); // Logging the time and date
+  $hostaddress = gethostbyaddr($ip); // Host name
+  $browser = $_SERVER['HTTP_USER_AGENT']; // needs work
+  $referred = $_SERVER['HTTP_REFERER']; // referrer
+
+           //Writing the time and date to a file
+
+  $myFile = "xxxx.txt";
+  $fh = fopen($myFile, 'a') or die("can't open file");
+  $stringData = "$time\n";
+  fwrite($fh, $stringData);
+  fclose($fh);
+
+          // writing Ip address to the same file
+
+  $myFile = "xxxx.txt"; // value 1
+  $fh = fopen($myFile, 'a') or die("can't open file");
+  $stringData = "$ip\n";
+  fwrite($fh, $stringData);
+  fclose($fh);
+
+          // also writing hostname to the same file
+
+
+  $myFile = "xxxx.txt"; // value 2
+  $fh = fopen($myFile, 'a') or die("can't open file");
+  $stringData = "$hostaddress\n";
+  fwrite($fh, $stringData);
+  fclose($fh);
+  print "<strong> </strong><br />\n";
+
+           if ($referred == "")
+              {
+                print "";
+               
+              }else{
+
+	             print "$referred";
+              }
+   ?>
+
+<!-- Redirectng to a different page (this is optional just delete this javascript if u dont want to redirect) -->
+
+<script type="text/javascript">
+   window.location = "http://jaysonzabate.com/"
+</script>
+  
+  </body>
+</html>
